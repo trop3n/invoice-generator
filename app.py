41c306e0466d331a914a05fe536d1e095620cd6f
trop3n <jasondkimm@proton.me>
2025-02-20 21:39:21 -0600

    initialized flask app

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from weasyprint import HTML
-import flask
+from flask import Flask
+import os
 
 html = HTML('invoice.html')
 html.write_pdf('invoice.pdf')
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 

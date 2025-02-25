86ee026ce820743213eab359a6a4f583ddfa7856
trop3n <jasondkimm@proton.me>
2025-02-25 10:55:38 -0600

    app.py updates

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from weasyprint import HTML
-from flask import Flask
+from flask import Flask, render_template
 import os
+from datetime import datetime
 
 html = HTML('invoice.html')
 html.write_pdf('invoice.pdf')
@@ -9,9 +10,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    today = datetime.today().strftime("%B %-d, %Y")
+    return render_template('invoice.html',
+                           date = today)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
